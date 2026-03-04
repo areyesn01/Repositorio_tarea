2bff1751a39917a2ee032999590ebe7853490feb
aereyesn01 <aereyesn01@iesalbarregas.es>
2026-03-04 09:17:45 +0100

    Actualización de los tres programas Java y configuración .gitignore

--- Main3.java
+++ Main3.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 
-public class Main {
+public class Main3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
